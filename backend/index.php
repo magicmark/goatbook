deb201b2dee4af9d16494bbb942c30e336e1f8ba
magicmark <mark@larah.me>
2013-10-26 19:12:55 +0100

    started the goatification process

--- backend/index.php
+++ backend/index.php
@@ -3,12 +3,15 @@ ini_set("memory_limit","128M");
 set_time_limit(0);
 include "image.php";
 
-$source   = new Image('mark.jpg');
-$overlay  = new Image('goathead.png');
+$source   = new Image('images/mark.jpg');
+$overlay  = new Image('images/goathead.png');
 
 $source->getFace();
+if (!isset($source->face['x'])){
+  $source->face['x'] = $source->size[0] / 3;
+  $source->face['y'] = $source->size[1] / 3;
+}
 
-header('Content-Type: image/jpeg');
 $canvas = imagecreatetruecolor($source->size[0], $source->size[1]);
 
 $faceOffset = array(
@@ -19,6 +22,8 @@ $facePos = array(
   'x' => $source->face['x'] + $faceOffset['x'],
   'y' => $source->face['y'] + $faceOffset['y'],
 );
+
+header('Content-Type: image/jpeg');
 
 imagecopy($canvas, $source->img, 0, 0, 0, 0, $source->size[0], $source->size[1]);
 imagecopy($canvas, $overlay->img, $facePos['x'], $facePos['y'], 0, 0, $overlay->size[0], $overlay->size[1]);
