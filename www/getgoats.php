707097942399481c76148bcc2234653c0f55857a
magicmark <mark@larah.me>
2013-10-26 23:15:22 +0100

    finishing get goats stuff

--- www/getgoats.php
+++ www/getgoats.php
@@ -12,5 +12,5 @@ $stmt = $pdo->prepare(
 $stmt->bindValue(1, $_GET['lastgoat'], PDO::PARAM_INT);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);  
-$json = json_encode($results[0]);
+$json = json_encode($results);
 echo $json;
