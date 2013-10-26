360810242adc9290fdf126726a235f369e2a88a8
RaduG <radu.ghitescu@gmail.com>
2013-10-26 15:33:42 +0100

    Added recieve_message.php

--- backend/recieve_message.php
+++ backend/recieve_message.php
@@ -1,6 +1,12 @@
- <?php
+<?php
 require 'class-Clockwork.php';
 $API_KEY = '873be9ec15eb56abca6c87d24da9199931090173';
+
+$from = $_POST['from'];
+$to = $_POST['to'];
+$content = $_POST['content'];
+$msg_id - $_POST['msg_id'];
+
 
 try
 {
@@ -8,7 +14,7 @@ try
     $clockwork = new Clockwork( $API_KEY );
  
     // Setup and send a message
-    $message = array( 'to' => '447716743842', 'from' => 'message' => 'This is a test!' );
+    $message = array( 'to' => $from, 'from' => $to => 'Here\'s your URL back: $content' );
     $result = $clockwork->send( $message );
  
     // Check if the send was successful
