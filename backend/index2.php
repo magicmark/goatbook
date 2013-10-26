b29f51c18b62dfe0b04e363ee7a9e0de1c9488d6
RaduG <radu.ghitescu@gmail.com>
2013-10-26 15:21:00 +0100

    Added clockwork

---
+++ backend/index2.php
@@ -0,0 +1,25 @@
+ <?php
+require 'class-Clockwork.php';
+$API_KEY = '873be9ec15eb56abca6c87d24da9199931090173';
+ 
+try
+{
+    // Create a Clockwork object using your API key
+    $clockwork = new Clockwork( $API_KEY );
+ 
+    // Setup and send a message
+    $message = array( 'to' => '447716743842', 'message' => 'This is a test!' );
+    $result = $clockwork->send( $message );
+ 
+    // Check if the send was successful
+    if($result['success']) {
+        echo 'Message sent - ID: ' . $result['id'];
+    } else {
+        echo 'Message failed - Error: ' . $result['error_message'];
+    }
+}
+catch (ClockworkException $e)
+{
+    echo 'Exception sending SMS: ' . $e->getMessage();
+}
+?>
